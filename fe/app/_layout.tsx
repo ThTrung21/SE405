4654fd84264ee240ab84c62eb275973d97100564
riz285 <hominhngan2852@gmail.com>
2025-03-15 14:33:56 +0700

    initialized

---
+++ fe/app/_layout.tsx
@@ -0,0 +1,26 @@
+// import { StatusBar} from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { Stack } from 'expo-router'
+
+const RootLayout: React.FC = () => {
+  return (
+    <>
+      <StatusBar style="auto" />
+      <Stack
+        screenOptions={{
+          headerStyle: { backgroundColor: '#ddd' },
+          headerTintColor: '#333',
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="index" options={{ title: 'Welcome' }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+        <Stack.Screen name="(stack)" options={{ headerShown: false }} />
+      </Stack>
+    </>
+  )
+}
+
+export default RootLayout
