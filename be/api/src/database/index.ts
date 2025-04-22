6696811b9046fe31d5f9fdbdf0f948c913e43d6e
TrungNg <ntrung1121@gmail.com>
2025-04-22 09:31:25 +0700

    init index for db

--- be/api/src/database/index.ts
+++ be/api/src/database/index.ts
@@ -1,12 +1,21 @@
 /* eslint-disable prettier/prettier */
-const { Sequelize } = require('sequelize');
-import { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PASS } from '@config';
+
+import { NODE_ENV, DB_HOST, DB_PORT, DB_USER, DB_DATABASE, DB_PASS } from '@config';
 
 import { logger } from '@/utils/logger';
-import { readFileSync } from 'fs';
-import { join } from 'path';
 
-console.log({ DB_DATABASE, DB_USER, DB_PASSWORD, DB_PASS });
+import BrandsModel from '@/models/brands.model';
+import CategoriesModel from '@/models/categories.model';
+import ConversationModel from '@/models/conversations.model';
+import MessageModel from '@/models/messages.model';
+import OrderItemModel from '@/models/order-items.model';
+import OrderModel from '@/models/orders.model';
+import ProductModel from '@/models/products.model';
+import ReviewsModel from '@/models/reviews.model';
+import UserModel from '@/models/users.model';
+import Sequelize from 'sequelize';
+
+// console.log({ DB_DATABASE, DB_USER, DB_PASSWORD, DB_PASS });
 
 const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASS, {
   dialect: 'mysql',
@@ -31,10 +40,88 @@ const sequelize = new Sequelize.Sequelize(DB_DATABASE, DB_USER, DB_PASS, {
   benchmark: true,
   attributeBehavior: 'unsafe-legacy',
 
-  dialectOptions: {
-    ssl: {
-      ca: readFileSync(join(__dirname, 'DigiCertGlobalRootCA.crt.pem')).toString(),
-    },
-  },
+  // dialectOptions: {
+  //   ssl: {
+  //     ca: readFileSync(join(__dirname, 'DigiCertGlobalRootCA.crt.pem')).toString(),
+  //   },
+  // },
 });
 sequelize.authenticate();
+
+const initAllModels = (sequelize: Sequelize.Sequelize) => {
+  const OrderItem = OrderItemModel(sequelize);
+  const Product = ProductModel(sequelize);
+  const Categories = CategoriesModel(sequelize);
+  const Order = OrderModel(sequelize);
+  const User = UserModel(sequelize);
+  const Brands = BrandsModel(sequelize);
+  const Message = MessageModel(sequelize);
+  const Conversation = ConversationModel(sequelize);
+  const Review = ReviewsModel(sequelize);
+
+  //table relations
+  Order.hasMany(OrderItem, { foreignKey: 'orderId' });
+  OrderItem.belongsTo(Order, { foreignKey: 'orderId' });
+
+  //brands and products
+  Brands.hasMany(Product, { foreignKey: 'brandId' });
+  Product.belongsTo(Brands, { foreignKey: 'brandId' });
+
+  //categories and products
+  Categories.hasMany(Product, { foreignKey: 'categoryId' });
+  Product.belongsTo(Categories, { foreignKey: 'categoryId' });
+
+  //products and an order_item
+  Product.hasMany(OrderItem, { foreignKey: 'productId' });
+  OrderItem.belongsTo(Product, { foreignKey: 'productId' });
+
+  //users and orders
+  User.hasMany(Order, { foreignKey: 'userId' });
+  Order.belongsTo(User, { foreignKey: 'userId' });
+
+  //review and products
+  Product.hasMany(Review, { foreignKey: 'productId' });
+  Review.belongsTo(Product, { foreignKey: 'productId' });
+
+  //review and user
+  User.hasMany(Review, { foreignKey: 'userId' });
+  Review.belongsTo(User, { foreignKey: 'userId' });
+
+  // Conversations and Users (customer and staff) and Orders
+  User.hasMany(Conversation, { foreignKey: 'userId', as: 'customerConversations' });
+  Conversation.belongsTo(User, { foreignKey: 'userId', as: 'customer' });
+
+  User.hasMany(Conversation, { foreignKey: 'staffId', as: 'staffConversations' });
+  Conversation.belongsTo(User, { foreignKey: 'staffId', as: 'staff' });
+
+  Order.hasMany(Conversation, { foreignKey: 'orderId' });
+  Conversation.belongsTo(Order, { foreignKey: 'orderId' });
+
+  // Messages and Conversations
+  Conversation.hasMany(Message, { foreignKey: 'conversationId' });
+  Message.belongsTo(Conversation, { foreignKey: 'conversationId' });
+  // Messages and Users (sender)
+  User.hasMany(Message, { foreignKey: 'senderId' });
+  Message.belongsTo(User, { foreignKey: 'senderId' });
+  //Messages and Products (optional)
+  Product.hasMany(Message, { foreignKey: 'productId' });
+  Message.belongsTo(Product, { foreignKey: 'productId' });
+
+  return {
+    OrderItem,
+    Product,
+    Categories,
+    Order,
+    User,
+    Brands,
+    Message,
+    Conversation,
+    Review,
+  };
+};
+
+export const DB = {
+  ...initAllModels(sequelize),
+  sequelize,
+  Sequelize,
+};
