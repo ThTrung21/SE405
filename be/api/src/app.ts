a63401769e1deda4b1eeaea98efd5b381a57bc4e
TrungNg <ntrung1121@gmail.com>
2025-05-13 11:14:28 +0700

    add route to server.ts

--- be/api/src/app.ts
+++ be/api/src/app.ts
@@ -1,4 +1,5 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
@@ -8,11 +9,12 @@ import hpp from 'hpp';
 import morgan from 'morgan';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
+import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS, HOST_NAME } from '@config';
 import { Routes } from '@interfaces/routes.interface';
 import { ErrorMiddleware } from '@middlewares/error.middleware';
 import { logger, stream } from '@utils/logger';
-
+import { DB } from '@/database';
+//
 export class App {
   public app: express.Application;
   public env: string;
@@ -23,6 +25,7 @@ export class App {
     this.env = NODE_ENV || 'development';
     this.port = PORT || 3000;
 
+    this.connectToDatabase();
     this.initializeMiddlewares();
     this.initializeRoutes(routes);
     this.initializeSwagger();
@@ -30,21 +33,37 @@ export class App {
   }
 
   public listen() {
-    this.app.listen(this.port, () => {
-      logger.info(`=================================`);
-      logger.info(`======= ENV: ${this.env} =======`);
-      logger.info(`🚀 App listening on the port ${this.port}`);
-      logger.info(`=================================`);
-    });
+    this.app
+      .listen(this.port, () => {
+        logger.info(`=================================`);
+        logger.info(`======= ENV: ${this.env} =======`);
+        logger.info(`🚀 App listening on http://${HOST_NAME}:${this.port}`);
+        logger.info(`=================================`);
+      })
+      .on('error', err => {
+        logger.error('Failed to start server:', err);
+        process.exit(1);
+      });
   }
 
   public getServer() {
     return this.app;
   }
-
+  private connectToDatabase() {
+    DB.sequelize
+      .sync({ alter: true })
+      .then(() => {
+        logger.info('Database connected!');
+      })
+      .catch(err => {
+        logger.error('Database connection failed:', err);
+        process.exit(1); // Exit the process with an error code
+      });
+  }
   private initializeMiddlewares() {
     this.app.use(morgan(LOG_FORMAT, { stream }));
-    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    //{ origin: ORIGIN, credentials: CREDENTIALS }
+    this.app.use(cors());
     this.app.use(hpp());
     this.app.use(helmet());
     this.app.use(compression());
@@ -63,7 +82,7 @@ export class App {
     const options = {
       swaggerDefinition: {
         info: {
-          title: 'REST API',
+          title: 'PetShop e-commerce',
           version: '1.0.0',
           description: 'Example docs',
         },
--- be/api/src/server.ts
+++ be/api/src/server.ts
@@ -2,9 +2,28 @@ import { App } from '@/app';
 import { AuthRoute } from '@routes/auth.route';
 import { UserRoute } from '@routes/users.route';
 import { ValidateEnv } from '@utils/validateEnv';
+import { ProductRoute } from '@routes/products.route';
+import { OrderRoute } from './routes/orders.route';
 
+import { CategoryRoute } from './routes/categories.route';
+import { OrderItemRoute } from './routes/order-items.route';
+// import { GeneralRoute } from './routes/general.route';
+import { BrandRoute } from './routes/brands.route';
+// import { DishRoute } from './routes/dishes.route';
+// import { RecommendationRoute } from './routes/recommendation.route';
 ValidateEnv();
-
-const app = new App([new UserRoute(), new AuthRoute()]);
+//[nodemon] app crashed - waiting for file changes before starting...
+const app = new App([
+  new AuthRoute(),
+  new UserRoute(),
+  new ProductRoute(),
+  new OrderRoute(),
+  new CategoryRoute(),
+  new OrderItemRoute(),
+  // new GeneralRoute(),
+  new BrandRoute(),
+  // new DishRoute(),
+  // new RecommendationRoute(),
+]);
 
 app.listen();
